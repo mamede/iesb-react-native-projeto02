6188e678b7c538e8d11fe8e431cabc4f0a7cd564
Felipe Mamede <felipebrasilmv@gmail.com>
2023-08-01 21:25:40 -0300

    :see_no_evil: add github user env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,2 +1,3 @@
 // Babel tava cagando e nao reconhecendo as envs entao deixei esse arquivo.
-export const GITHUB_TOKEN = "TOKEN_AQUI";
+export const GITHUB_TOKEN = "GITHUB_TOKEN";
+export const GITHUB_USER = "GITHUB_USER"
